168e4aebbc3a0526e6aaf2dddb5e90746ea931b2
suman <srigadasuman18@gmail.com>
2023-11-14 21:38:58 +0530

    new one upload

---
+++ src/componants/HeaderNav.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {Link} from'react-router-dom';
+
+
+const HeaderNav = () => {
+  return (
+    <div>
+    <div className='navbar'>
+        <Link to='/b' className='main-title'>Cooltree</Link>
+   <ul >
+        <Link to='/'><li>Home</li></Link>
+        <Link to='/Home'><li>Profile</li></Link>
+        <Link to='/signup'><li>Contact</li></Link>
+        <Link to='/about'><li>About</li></Link>
+        <Link to='/signin'><li>SignIn</li></Link>
+        </ul>
+    </div>
+    <div className='leftcontainer'>
+      <div className='leftbar'>
+    <ul >
+        <Link to='/'><li>International</li></Link>
+        <Link to='/Home'><li>National</li></Link>
+        <Link to='/signup'><li>State</li></Link>
+        <Link to='/about'><li>Hyderabad</li></Link>
+        <Link to='/signin'><li>Politics</li></Link>
+     
+        
+        
+        </ul>
+        </div>
+  
+    <div className='photo'>
+      
+        <img src='/images/new year.jpg'></img>
+    </div>
+  </div>
+    </div>
+  )
+}
+
+export default HeaderNav
