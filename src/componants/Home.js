f63e01825b3f3646db821aa54272686f4121aa9e
suman <srigadasuman18@gmail.com>
2023-12-01 17:47:19 +0530

    the new one

--- src/componants/Home.js
+++ src/componants/Home.js
@@ -1,26 +1,32 @@
 import React from 'react'
 import './home.css';
+
+import Button from 'react-bootstrap/Button';
+import {useNavigate} from'react-router-dom';
 const Home = () => {
   return (
     <div className='homecontainer'>
       <div className='homebox'>
       <div className='first'>
       <img src='/homepage/wonders.jpg'></img>
-      <h3>ప్రపంచ వింతలు</h3>
-      <img src='/homepage/wonders.jpg'></img>
-      <h3>ప్రపంచ వింతలు</h3>
+      <Button variant='danger' >ప్రపంచ వింతలు</Button>
+
+      <img src='/calendar/jan.jpeg'></img>
+      <Button variant='warning' >2024 క్యాలెండర్</Button>
+      
       </div>
       <div className='second'>
-      <img src='/homepage/wonders.jpg'></img>
-      <h3>ప్రపంచ వింతలు</h3>
-      <img src='/homepage/wonders.jpg'></img>
-      <h3>ప్రపంచ వింతలు</h3>
+      <img src='/homepage/moralstory.jpg'></img>
+      <Button variant='warning' >నీతి కథలు</Button>
+      <img src='/homepage/scheme.jpg'></img>
+      <Button variant='info' >కేంద్ర పథకాలు</Button>
       </div>
       <div className='third'>
+      <img src='/homepage/pradeshalu.jpg'></img>
+      <Button variant='info' >ప్రదేశాలు</Button>
+
       <img src='/homepage/wonders.jpg'></img>
-      <h3>ప్రపంచ వింతలు</h3>
-      <img src='/homepage/wonders.jpg'></img>
-      <h3>ప్రపంచ వింతలు</h3>
+      <Button variant='info' >ప్రపంచ వింతలు</Button>
       </div>
       </div>
         
--- src/componants/Sidebar.js
+++ src/componants/Sidebar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import {useNavigate} from'react-router-dom';
+import './home.css';
 
 
 
